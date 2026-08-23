520c06d9f296309e2e3c269892f46ce956d4fc79
Kalonji <kalonji@example.com>
2026-08-23 20:31:27 +0100

    Bump_version_to_45_0_0

--- config/config.js
+++ config/config.js
@@ -103,7 +103,7 @@ const config = {
   // Application Constants
   constants: {
     appName: 'Meet Me',
-    appVersion: '44.0.0',
+    appVersion: '45.0.0',
     defaultLanguage: 'fr',
     supportedLanguages: ['fr', 'en', 'es', 'de', 'it', 'pt', 'ru', 'zh', 'ja', 'ko', 'ar'],
     messageTypes: {
